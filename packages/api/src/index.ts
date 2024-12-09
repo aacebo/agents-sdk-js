e03cecde36514df0fe4d1dad244e8d0640581d77
aacebo <aacebowork@gmail.com>
2024-12-10 05:20:21 +0900

    Update index.ts

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,8 @@ import http from 'node:http';
 import express from 'express';
 import debug from 'debug';
 import cors from 'cors';
-import socketio from 'socket.io';
+import socket from 'socket.io';
+import socketClient from 'socket.io-client';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
@@ -12,6 +13,11 @@ import pkg from '../package.json';
 
 const argv = yargs(hideBin(process.argv))
   .version(pkg.version)
+  .option('port', {
+    type: 'number',
+    describe: 'which port to run on',
+    default: 3000
+  })
   .option('name', {
     type: 'string',
     describe: 'the agents name',
@@ -27,32 +33,59 @@ const argv = yargs(hideBin(process.argv))
     describe: 'the LLM model to use',
     demandOption: true
   })
+  .option('api-key', {
+    type: 'string',
+    describe: 'the API key to use for the LLM',
+    demandOption: true
+  })
+  .option('parent', {
+    type: 'string',
+    describe: 'the parent agents url to connect to'
+  })
   .parseSync();
 
-const port = process.env.PORT || 3000;
 const app = express();
 const log = debug(`agents/${argv.name}`);
 log.enabled = true;
 
+const sockets: Array<string> = [];
+
 app.use(express.json());
 app.use(cors());
-
 app.get('/', (_, res) => {
   res.json({
     name: argv.name,
     prompt: argv.prompt,
-    model: argv.model
+    model: argv.model,
+    parent: argv.parent,
+    sockets
   });
 });
 
 const server = http.createServer(app);
-const io = new socketio.Server(server);
-
-io.on('connection', client => {
-  client.on('event', () => log('event received...'));
-  client.on('disconnect', () => log('disconnected...'));
+const io = new socket.Server(server, {
+  cors: { origin: '*' }
 });
 
-app.listen(port, () => {
-  log(`🚀 listening on port ${port}...`);
+io.on('connection', socket => {
+  const name = socket.handshake.auth.name;
+  sockets.push(name);
+  log(`${name} connected...`);
+});
+
+if (!!argv.parent) {
+  const socket = socketClient(argv.parent, {
+    auth: { name: argv.name },
+    autoConnect: false
+  });
+
+  socket.on('connect', () => {
+    log('connected to parent...');
+  });
+
+  socket.connect();
+}
+
+server.listen(argv.port, () => {
+  log(`🚀 listening on port ${argv.port}...`);
 });
