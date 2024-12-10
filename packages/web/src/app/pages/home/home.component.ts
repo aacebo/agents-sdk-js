7f1b770dbaf5e5ad9f947878fa0570af96f9ecf7
aacebo <aacebowork@gmail.com>
2024-12-11 08:02:01 +0900

    stuff

--- packages/web/src/app/pages/home/home.component.ts
+++ packages/web/src/app/pages/home/home.component.ts
@@ -1,14 +1,24 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { IconModule } from '../../components/icon';
+import { AppSocket } from '../../app.socket';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   host: { class: 'app-home' },
-  imports: [IconModule],
+  imports: [FormsModule, IconModule],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Home { }
+export class Home {
+  text = '';
+
+  constructor(private readonly _socket: AppSocket) { }
+
+  send() {
+    this._socket.send(this.text);
+  }
+}
