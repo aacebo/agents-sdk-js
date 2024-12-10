2c7a47f3e14344eb3558d50c86cd3ea293b01b08
aacebo <aacebowork@gmail.com>
2024-12-11 05:46:26 +0900

    stuff

--- packages/web/src/app/app.component.ts
+++ packages/web/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'web';
+export class AppComponent implements OnInit {
+  readonly state = new BehaviorSubject<any>(null);
+
+  constructor(private readonly _http: HttpClient) { }
+
+  async ngOnInit() {
+    const res = await firstValueFrom(this._http.get<any>('/health'));
+    this.state.next(res);
+  }
 }
