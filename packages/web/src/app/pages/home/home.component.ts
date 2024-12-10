99bc24c3fa92f14e176a4c4bfd9db956a9266273
aacebo <aacebowork@gmail.com>
2024-12-11 07:55:24 +0900

    stuff

--- packages/web/src/app/pages/home/home.component.ts
+++ packages/web/src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { IconModule } from '../../components/icon';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   host: { class: 'app-home' },
+  imports: [IconModule],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
